8bcd39abc61a6fb3ed27a31021727b5ab14f2575
nataliereshetnikova <ab1748@gmail.com>
2019-06-21 09:32:04 -0400

    Updates

---
+++ precache-manifest.82afa7780f951587bd5c2734d6f969b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0a2bc16d899c44eb6dd06506b6add7",
+    "url": "/React-sparkling-string-app/index.html"
+  },
+  {
+    "revision": "d3fa4b7d78db18ac7bb2",
+    "url": "/React-sparkling-string-app/static/css/main.6f8a2c5b.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/React-sparkling-string-app/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "d3fa4b7d78db18ac7bb2",
+    "url": "/React-sparkling-string-app/static/js/main.49badd75.chunk.js"
+  },
+  {
+    "revision": "611063a09b84ef1b8680",
+    "url": "/React-sparkling-string-app/static/js/runtime~main.0c055193.js"
+  }
+]);
